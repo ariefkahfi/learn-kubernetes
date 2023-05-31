2bf5dd22ad51c367133474abc072de7b89342603
ariefkahfi <ariefkahfi@gmail.com>
2023-05-31 16:59:23 +0700

    implement rails with database and jobs

--- nginx-with-rails/blog/app/controllers/hellos_controller.rb
+++ nginx-with-rails/blog/app/controllers/hellos_controller.rb
@@ -2,7 +2,8 @@ class HellosController < ApplicationController
   def index
     ActiveRecord::Base.establish_connection # Establishes connection
     ActiveRecord::Base.connection # Calls connection object
-    render plain: 'CONNECTED!' if ActiveRecord::Base.connected?
+    current_hostname = `hostname`
+    render plain: "from: #{current_hostname}, CONNECTED!" if ActiveRecord::Base.connected?
     render plain: 'NOT CONNECTED!' unless ActiveRecord::Base.connected?
   rescue StandardError
     render plain: 'NOT CONNECTED!'
